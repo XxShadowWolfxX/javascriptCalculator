069d998d770a3344a97d718e2e05027001b34b94
XxShadowWolfxX <alim.0158@gmail.com>
2017-04-11 20:54:33 -0400

    Init Files

---
+++ calc.js
@@ -0,0 +1,22 @@
+// Functions 
+
+function add() {
+	// console.log("Let's add!");
+	var math = 7 + 7;
+	console.log(math);
+}
+function subtract() {
+	// console.log("Time to subtract!");
+	var math = 7 - 7;
+	console.log(math);
+}
+function multiply() {
+	// console.log("Du du du, multiplication means complications");
+	var math = 7 * 7;
+	console.log(math);
+}
+function divide() {
+	// console.log("Division my vision.. I don't know, I'm running out of material");
+	var math = 7 / 7;
+	console.log(math);
+}
